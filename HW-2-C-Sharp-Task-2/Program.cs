70cda5927c56216f0674fa818ccd6d5fea5fdb06
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2022-06-20 15:59:22 +0300

    Добавьте файлы проекта.

---
+++ HW-2-C-Sharp-Task-2/Program.cs
@@ -0,0 +1,63 @@
+﻿//Задание 2
+//Дан двумерный массив размерностью 5×5, заполненный случайными числами из диапазона от –100 до 100.
+//Определить сумму элементов массива, расположенных между минимальным и максимальным элементами.
+
+using System;
+
+namespace HW_2_C_Sharp_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int row = 5;
+            int col = 5;
+            int rndMin = -100;
+            int rndMax = 100;
+            int min = rndMax;
+            int max = rndMin;
+            int summ = 0;
+            int[,] arrMinMaxItem = new int[2, 2];
+            int[,] arr = new int[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arr[i, j] = random.Next(rndMin, rndMax);
+                    Console.Write($"{arr[i, j]}. ");
+                    if (min > arr[i, j])
+                    {
+                        min = arr[i, j];
+                        arrMinMaxItem[0, 0] = i;
+                        arrMinMaxItem[0, 1] = j;
+                    }
+                    if (max < arr[i, j])
+                    {
+                        max = arr[i, j];
+                        arrMinMaxItem[1, 0] = i;
+                        arrMinMaxItem[1, 1] = j;
+                    }
+                }
+                Console.WriteLine();
+            }
+            int minRow = (arrMinMaxItem[0, 0] < arrMinMaxItem[1, 0]) ? arrMinMaxItem[0, 0] : arrMinMaxItem[1, 0];
+            int maxRow = (arrMinMaxItem[0, 0] > arrMinMaxItem[1, 0]) ? arrMinMaxItem[0, 0] : arrMinMaxItem[1, 0];
+            int minCol = (arrMinMaxItem[0, 1] < arrMinMaxItem[1, 1]) ? arrMinMaxItem[0, 1] : arrMinMaxItem[1, 1];
+            int maxCol = (arrMinMaxItem[0, 1] > arrMinMaxItem[1, 1]) ? arrMinMaxItem[0, 1] : arrMinMaxItem[1, 1];
+            for (int i = minRow; i < row; i++)
+            {
+                for (int j = minCol; j < col; j++)
+                {
+                    if (i > maxRow && j > maxCol)
+                        continue;
+                    summ +=arr[i, j];
+
+                }
+            }
+            Console.WriteLine($"min = {min} with i = {arrMinMaxItem[0, 0]+1} and j = {arrMinMaxItem[0, 1]+1}");
+            Console.WriteLine($"max = {max} with i = {arrMinMaxItem[1, 0]+1} and j = {arrMinMaxItem[1, 1]+1}");
+            Console.WriteLine($"Summ = {summ}");
+        }
+    }
+}
